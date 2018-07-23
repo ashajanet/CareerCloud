c2c55a13e67958471ab5afff7b1ff7d080203ca3
AshaJanet <AshaJanet.MA@gmail.com>
2018-07-23 16:04:13 -0400

    Added Get GetAll method to SystemLanguageCode and SystemCountryCode

--- CareerCloud.BusinessLogicLayer/SystemCountryCodeLogic.cs
+++ CareerCloud.BusinessLogicLayer/SystemCountryCodeLogic.cs
@@ -5,47 +5,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CareerCloud.BusinessLogicLayer
 {
-    public class SystemCountryCodeLogic 
+    public class SystemCountryCodeLogic
     {
+        protected IDataRepository<SystemCountryCodePoco> _repository;
+
         public SystemCountryCodeLogic(IDataRepository<SystemCountryCodePoco> repository)
-            
-        { }
+        { _repository = repository; }
+
+        public SystemCountryCodePoco Get(string code)
+        {
+            return _repository.GetSingle(c => c.Code == code);
+        }
+
+        public List<SystemCountryCodePoco> GetAll()
+        {
+            IList<SystemCountryCodePoco> pocos = _repository.GetAll().ToList();
+            return pocos.ToList();
+        }
         public void Add(SystemCountryCodePoco[] pocos)
         {
             Verify(pocos);
-            
+            _repository.Add(pocos);
         }
+
         public void Update(SystemCountryCodePoco[] pocos)
         {
             Verify(pocos);
-            
+            _repository.Update(pocos);
         }
         protected void Verify(SystemCountryCodePoco[] pocos)
         {
             List<ValidationException> exceptions = new List<ValidationException>();
-
             foreach (SystemCountryCodePoco poco in pocos)
             {
                 if (string.IsNullOrEmpty(poco.Code))
                 {
-                    exceptions.Add(new ValidationException(900, $"Cannot be empty - {poco.Code}"));
+                    exceptions.Add(new ValidationException(900,
+                        $"Cannot be empty "));
                 }
                 if (string.IsNullOrEmpty(poco.Name))
                 {
-                    exceptions.Add(new ValidationException(901, $"Cannot be empty - {poco.Code}"));
+                    exceptions.Add(new ValidationException(901,
+                        $"Cannot be empty "));
                 }
-
-                if (exceptions.Count > 0)
-                {
-                    throw new AggregateException(exceptions);
-                }
-
-
             }
-
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
         }
     }
 }
--- CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
+++ CareerCloud.BusinessLogicLayer/SystemLanguageCodeLogic.cs
@@ -6,52 +6,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CareerCloud.BusinessLogicLayer
 {
-    public class SystemLanguageCodeLogic 
+    public class SystemLanguageCodeLogic
     {
+        protected IDataRepository<SystemLanguageCodePoco> _repository;
         public SystemLanguageCodeLogic(IDataRepository<SystemLanguageCodePoco> repository)
-            
-        { }
+        { _repository = repository; }
+
+        public SystemLanguageCodePoco Get(string code)
+        {
+            return _repository.GetSingle(c => c.LanguageID == code);
+        }
+
+        public List<SystemLanguageCodePoco> GetAll()
+        {
+            IList<SystemLanguageCodePoco> pocos = _repository.GetAll().ToList();
+            return pocos.ToList();
+        }
+
         public void Add(SystemLanguageCodePoco[] pocos)
         {
             Verify(pocos);
-           
+            _repository.Add(pocos);
         }
+
         public void Update(SystemLanguageCodePoco[] pocos)
         {
             Verify(pocos);
-            
+            _repository.Update(pocos);
         }
-        protected  void Verify(SystemLanguageCodePoco[] pocos)
+        protected void Verify(SystemLanguageCodePoco[] pocos)
         {
             List<ValidationException> exceptions = new List<ValidationException>();
-
             foreach (SystemLanguageCodePoco poco in pocos)
             {
                 if (string.IsNullOrEmpty(poco.LanguageID))
                 {
-                    exceptions.Add(new ValidationException(1000, $"Cannot be empty - {poco.LanguageID}"));
+                    exceptions.Add(new ValidationException(1000,
+                        $"Cannot be empty "));
                 }
                 if (string.IsNullOrEmpty(poco.Name))
                 {
-                    exceptions.Add(new ValidationException(1001, $"Cannot be empty - {poco.LanguageID}"));
+                    exceptions.Add(new ValidationException(1001,
+                        $"Cannot be empty "));
                 }
                 if (string.IsNullOrEmpty(poco.NativeName))
                 {
-                    exceptions.Add(new ValidationException(1002, $"Cannot be empty - {poco.LanguageID}"));
+                    exceptions.Add(new ValidationException(1002,
+                        $"Cannot be empty "));
                 }
-
-                if (exceptions.Count > 0)
-                {
-                    throw new AggregateException(exceptions);
-                }
-
-
             }
-
+            if (exceptions.Count > 0)
+            {
+                throw new AggregateException(exceptions);
+            }
         }
     }
-
-
 }
